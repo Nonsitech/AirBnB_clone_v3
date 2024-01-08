c0f70c9b03ecc63e73842020235a9feeba9ab0bc
UmarMsani <umuhammad220@gmail.com>
2024-01-08 15:49:01 -0800

    AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,11 @@ app = Flask(__name__)
 # Register the blueprint with the Flask app
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+# Task 12
+# Enabling CORS and allow request from the origin:
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
@@ -26,17 +31,20 @@ def teardown_engine(exception):
 # error handler for 404 Not Found
 @app.errorhandler(400)
 def not_found(error):
-    '''Handles the 400 HTTP error code.'''
+    """Handling the 400 HTTP error code"""
     response = {'error': 'Not found'}
     return jsonify(response), 404
 
-
-# Task 12
-# Enabling CORS and allow request from the origin:
-CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0.'}})
+@app.errorhandler(400)
+def error_400(error):
+    """Handling the 400 HTTP error code"""
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=app_host, port=app_port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -25,12 +25,12 @@ def api_status():
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
     """Retrieves the number of each objects by type"""
-    stats {
-            'aminities': storage.count('Aminity'),
-            'cities': storage.count('City'),
-            'palaces': storage.count('Place'),
-            'reviews': storage.count('Review'),
-            'states': storage.count('State'),
-            'users': storage.count('Usuer')
-            }
+    atats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
     return jsonify(stats)
