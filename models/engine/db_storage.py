3581ab985cd533894422c96f1697cdc1745bf4e7
UmarMsani <umuhammad220@gmail.com>
2024-01-08 01:20:03 +0100

    AirBnB clone - RESTful API

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,24 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns object based on the class name and its ID, or
+        None if not found
+        """
+        if cls not in classes.values() and id and type(id) == str:
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+    
+    def count(self, cls=None):
+        """
+        count the number of objects in storage
+        """
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
